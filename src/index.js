190f346d118ce162da3c64bab7839b486bf9a830
DE36 <veeresh.kashappa@in.tesco.com>
2017-07-21 00:03:03 +0530

    latest

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom";
 import Sidebar from './Sidebar.jsx';
 
-ReactDOM.render(
-<Sidebar />,
-    document.getElementById('app')
-);
+export default Sidebar;
